c7e005410e21c11f916115becea67c95be3ad3bc
SondreB <sondre@outlook.com>
2025-03-24 22:42:41 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/sondreb/contract-society/commit/2615239dbc444f092f79455b43941b61fde61028

--- chunk-XO6TUDXF.js
+++
@@ -1,1 +0,0 @@
-import{Dc as r,Kb as n,Lb as i,_b as p,mb as t}from"./chunk-F2AWL3FP.js";var m=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=t({type:o,selectors:[["app-profile"]],decls:2,vars:0,template:function(e,s){e&1&&(n(0,"p"),p(1,"Profile component works!"),i())},dependencies:[r],encapsulation:2})};export{m as ProfileComponent};
