2615239dbc444f092f79455b43941b61fde61028
SondreB <sondre@outlook.com>
2025-03-24 23:41:57 +0100

    Add a test link for Sunce Wallet

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -5,7 +5,13 @@ import { CommonModule } from '@angular/common';
   selector: 'app-profile',
   standalone: true,
   imports: [CommonModule],
-  template: '<p>Profile component works!</p>',
+  template: `<p>Profile component works!</p>
+  
+  <a href="
+  web+stellar:pay?destination=GDMBM7P2ZVD64DSMQJIR67CZFWU7EQRI4YMRLZ2XOYT3V7YUBGZ4RXHF&amount=1&asset_code=EURMTL&asset_issuer=GACKTN5DAZGWXRWB2WLM6OPBDHAMT6SJNGLJZPQMEZBUR4JUGBX2UK7V&memo=sign&memo_type=text">Login with Sunce Wallet</a>
+  
+  
+  `,
   styles: ''
 })
 export class ProfileComponent {}
