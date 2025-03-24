7027b9acaa864793ce437fcc7cf71424dd6dc44c
SondreB <sondre@outlook.com>
2025-03-24 23:11:20 +0100

    Add service worker support and update manifest for PWA capabilities

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { MAT_CARD_CONFIG } from '@angular/material/card';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +13,9 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     importProvidersFrom(BrowserAnimationsModule),
     { provide: MAT_CARD_CONFIG, useValue: { appearance: 'outlined' } },
-    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } }
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 };
